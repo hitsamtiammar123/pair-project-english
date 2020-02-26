64e511051a45bdd0d8092afa2ddcdc350c901642
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-02-26 23:20:28 +0700

    comment balik session

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const PORT=process.env.PORT||3000;
 app.set('view engine','ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(express.urlencoded({extended:true}));
-// app.use(session({
-//     secret: 'hehehehe',
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: true }
-// }));
+app.use(session({
+    secret: 'hehehehe',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
 
 app.get('/',(req,res)=>{
-    res.send('Ini halaman landing page untuk Pair Project');
+    res.send('Ini halaman landing page untuk Pair Project ');
 });
 
 app.listen(PORT,()=>{
