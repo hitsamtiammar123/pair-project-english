366fc613fab5d15868ccfcc4d5f93118ddfc7c85
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-02-27 21:14:02 +0700

    update course all dan detail

--- controller/courseController.js
+++ controller/courseController.js
@@ -1,10 +1,22 @@
-const{Courses}=require("../models/index");
+const{Course}=require("../models/index");
 class Controller{
 
     index(req,res){
        
     }
-    
+
+    all(req,res){
+        Course.findAll()
+        .then((courses)=>res.render('ejs/courses',{session:req.session,courses}))
+        .catch((err)=>res.send(err))
+    }
+
+    courseDetail(req,res){
+        let id=Number(req.params.id);
+        Course.findByPk(id)
+        .then((course)=>res.render('ejs/course-detail',{session:req.session,course}))
+        .catch((err)=>res.send(err))
+    }
     
 }
 module.exports=Controller;
--- router/routerCourses.js
+++ router/routerCourses.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controller/courseController");
+const Controller = require("../controller/courseController");
 
+const controller=new Controller();
 
+router.get('/all',controller.all);
+router.get('/detail/:id',controller.courseDetail);
 
 module.exports=router;
