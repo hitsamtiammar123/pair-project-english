2dff6ff0c2ae37b9f050adac890ea5e43b30fed3
verypei <verrymariyanto@gmail.com>
2020-02-27 19:58:49 +0700

    findAll

--- controller/courseController.js
+++ controller/courseController.js
@@ -2,7 +2,15 @@ const{Courses}=require("../models/index");
 class Controller{
 
     index(req,res){
-        
+        Courses.findAll({
+            includes:{questions}
+        })
+        .then(data=>{
+            res.send(data);
+        })
+        .catch(err=>{
+            res.send(err)
+        })
     }
     
     
