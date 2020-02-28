a7bc49bbf5780040e5adcd2560ed4fe55c5a51d6
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-02-28 07:19:20 +0700

    ubah settingan session

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(session({
     secret: 'hehehehe',
     resave: true,
     saveUninitialized: true,
-    cookie: { secure: false }
+    cookie: { secure: true }
 }));
 
 app.use('/',require('./router/routerindex'));
